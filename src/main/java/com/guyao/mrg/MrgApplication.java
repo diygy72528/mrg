4fb742d060008eac06dd1f3be9ff4d90e1bcbbdd
little <1106222833@qq.com>
2019-07-11 15:42:02 +0800

    Initial commit

---
+++ src/main/java/com/guyao/mrg/MrgApplication.java
@@ -0,0 +1,13 @@
+package com.guyao.mrg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MrgApplication.class, args);
+    }
+
+}
